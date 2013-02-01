fcfe8939b49908bdeb281fa493823d4d7a844da3
Daniel Nimmervoll <daniel@nimmervoll.eu>
2013-02-01 20:46:00 +0100

    Update install.sh
    ...

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ DIR=`pwd`
 
 USR='whoami'
 
-mv $DIR/acat.py /opt/acat.py
+mv $DIR/acat.py /opt/
 
 if [ "$(whoami)" != "root" ]
 then
-echo alias acat='python /opt/acat.py' >> /home/$USR/.bashrc
+echo "alias acat='python /opt/acat.py' >> /home/$USR/.bashrc"
 else 
-echo alias acat='python /opt/acat.py' >> /root/.bashrc
+echo "alias acat='python /opt/acat.py' >> /root/.bashrc"
 exit 1
 fi
