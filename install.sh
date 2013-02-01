5b22bf36632b545bf65379909cb518847bc0554b
Daniel Nimmervoll <daniel@nimmervoll.eu>
2013-02-01 20:41:15 +0100

    Update install.sh
    changed error

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-DIR='pwd'
+DIR=´pwd`
+
 USR='whoami'
 
 mv $DIR/acat.py /opt/acat.py
 
 if [ "$(whoami)" != "root" ]
 then
-echo alias acat='python /path/to/acat.py' >> /home/$USR/.bashrc
+echo alias acat='python /opt/acat.py' >> /home/$USR/.bashrc
 else 
-echo alias acat='python /path/to/acat.py' >> /root/.bashrc
+echo alias acat='python /opt/acat.py' >> /root/.bashrc
 exit 1
 fi
